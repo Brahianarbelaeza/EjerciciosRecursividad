ad7d984d7c8d1a244374af5948440db80b1d1cd5
brahian andres arbelaez aguirre <brahiana.arbelaeza@uqvirtual.edu.co>
2023-03-05 14:17:00 -0500

    se terminaron los ejercicios

--- src/EjercicioCinco.java
+++ src/EjercicioCinco.java
@@ -1,24 +1,31 @@
 public class EjercicioCinco {
     //5.Retornar el número de la fila de la matriz cuya suma de todos sus elementos sea mayor.
     public static void main(String[] args) {
-        int[][] m = {{1, 2, 321}, {4, 5, 6}, {7, 8, 9}};
-        int filamayor= filaMayorSuma(m,0,0,0)+1;
+        int[][] m = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int filamayor= filaMayorSuma(m,0,0,0,0,0)+1;
         System.out.println("la fila con la suma mayor es :"+filamayor);
     }
 
 
-    public static int filaMayorSuma(int[][] m, int i, int j, int mayorsuma, int filamayor) {
-        int sumaFila = 0;
-        if (j==m[i].length)
-
-        if (sumaFila > mayorsuma) {
+    public static int filaMayorSuma(int[][] m, int i, int j, int mayorsuma, int filamayor, int sumaFila) {
+        if (j==m[i].length-1){
+            sumaFila += m[i][j];
+            if(mayorsuma < sumaFila){
                 mayorsuma = sumaFila;
                 filamayor = i;
-        } if (i == m.length - 1) {
-            return filamayor;
-        }else {
-            return filaMayorSuma(m,i+1, mayorsuma, filamayor);
+                sumaFila = 0;
+                if (i == m.length - 1 ){
+                    return filamayor;
+                }
+            }
+            j=0;
+            i++;
+        } else {
+            sumaFila += m[i][j];
+            j++;
         }
+        return filaMayorSuma(m, i , j, mayorsuma, filamayor,sumaFila);
+
     }
 }
 
--- src/EjercicioSeis.java
+++ src/EjercicioSeis.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+
 public class EjercicioSeis {
+    //66. Dada una matriz de String de 4 x 4 conformada por palabras, hallar las palabras que tienen dos vocales seguidas de forma recursiva y agregarlas a un ArrayList. Ejemplo de la matriz a probaren el archivo adjunto.
+    //
+    //
+    //Para recorrer la matriz se debe hacer de forma recursiva. Función a utilizar  recorrerMatriz(matriz,i,j, listaPalabras)
+    //
+    //Para verificar si la palabra tiene 2 vocales seguidas se debe hacer de forma recursiva. Funciona a utilizar verificarVocalesSeguidas(identificar los parámetros que necesita)
+    public static void main(String[] args) {
+        String[][] m = {{"vacio", "carro", "baul", "perro"}, {"colombia", "casa", "moto", "caza"}, {"llanta", "reir", "archivo", "silla"}, {"cocina", "ola", "ave", "freir"}};
+        ArrayList<String> listaPalabras = new ArrayList<>();
+        recorrerMatriz(m, 0, 0, listaPalabras);
+        System.out.println(listaPalabras);
+    }
+
+     public static ArrayList<String> recorrerMatriz(String[][] m, int i, int j, ArrayList<String> listaPalabras) {
+         if (i == m.length || j == m[i].length) {
+             return listaPalabras;
+         } else {
+             if (verificarVocalesSeguidas(m[i][j],0) == true) {
+                 listaPalabras.add(m[i][j]);
+             }
+             if (j == m[i].length - 1) {
+                 i++;
+                 j = 0;
+             } else {
+                 j++;
+             }
+             return recorrerMatriz(m, i, j, listaPalabras);
+         }
+
+     }
+
+    private static boolean verificarVocalesSeguidas(String palabra, int indice) {
+        if (indice == palabra.length() - 1) {
+            return false;
+        } else {
+            if (esVocal(palabra.charAt(indice)) && esVocal(palabra.charAt(indice + 1))) {
+                return true;
+            } else {
+                return verificarVocalesSeguidas(palabra, indice + 1);
+            }
+        }
+    }
+
+    private static boolean esVocal(char letra) {
+        return (letra== 'a' || letra== 'e' || letra== 'i'|| letra=='o' || letra == 'u');
+    }
 }
